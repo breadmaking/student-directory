fa7de69dd2f135096a0d2c2efc4287e12aebad47
breadmaking <liquidgoldlabel@gmail.com>
2022-01-18 09:46:16 +0000

    Array and puts refactoring

--- directory.rb
+++ directory.rb
@@ -1,17 +1,23 @@
-student_count = 11
-#first we print the list of students
+#first we print the list of students - put into an array
+students = [
+  "The students of Villains Academy",
+  "-------------",
+  "Dr. Hannibal Lecter",
+  "Darth Vader",
+  "Nurse Ratched",
+  "Michael Corleone",
+  "Alex DeLarge",
+  "The Wicked Witch of the West",
+  "The Terminator",
+  "Freddy Kruger",
+  "The Joker",
+  "Joffery Baratheon",
+  "Norman Bates",
+]
 puts "The students of Villains Academy"
 puts "-------------"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "The Terminator"
-puts "Freddy Kruger"
-puts "The Joker"
-puts "Joffery Baratheon"
-puts "Norman Bates"
+students.each do |student|
+  puts students
+end
 #finally we print the total number of students
-print "Overall, we have #{student_count} great students"
+print "Overall, we have #{students.count} great students"

e69c0c6479c36af137346bff121b94848c694cd2
breadmaking <liquidgoldlabel@gmail.com>
2022-01-18 10:09:27 +0000

    get user inout for student list

--- directory.rb
+++ directory.rb
@@ -1,17 +1,19 @@
 #first we print the list of students - put into an array
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "The Terminator", cohort: :november},
-  {name: "Freddy Kruger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffery Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november},
-]
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+
+  students = []
+
+  name = gets.chomp
+
+  while !name.empty? do
+    students << {name:name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    name = gets.chomp
+  end
+  students
+end
 
 def print_header
   puts "The students of Villains Academy"
@@ -29,6 +31,7 @@ def print_footer(students)
 end
 
 #call the methods
+students = input_students
 print_header
 print(students)
 print_footer(students)

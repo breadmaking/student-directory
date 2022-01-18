ffbc1cdc5b32f87fb1fd725bfdaa411cb084ec8c
breadmaking <liquidgoldlabel@gmail.com>
2022-01-18 16:17:45 +0000

    Step11

--- directory.rb
+++ directory.rb
@@ -1,13 +1,15 @@
+@students = []
+
 def print_header
   puts "The students of Villains Academy".center(75,'---')
   puts "-------------".center(75,'---')
 end
 
-def print(students)
-  if students.count == 0
+def print
+  if @students.count != 0
     all_cohorts = []
 
-    students.each do |student|
+    @students.each do |student|
       all_cohorts << student[:cohort].capitalize
     end
 
@@ -15,22 +17,20 @@ def print(students)
 
     all_cohorts.each do |cohort|
       puts "Cohort: #{cohort}"
-      students.each do |student|
+      @students.each do |student|
         if student[:cohort].capitalize == cohort
           puts "#{student[:name]}. Hobbies: #{student[:hobbies].join(', ')}. Birthplace: #{student[:birthplace]}. Age: #{student[:age]}."
         end
       end
     end
-  else
-    puts 'No Students'
   end
 end
 
-def print_footer(students)
-  if students.count == 1
-    puts "Overall, we have #{students.count} great student"
+def print_footer
+  if @students.count >= 1
+    puts "Overall, we have #{@students.count} great student"
   else
-    puts "We have #{students.count} students"
+    puts "We have #{@students.count} students"
   end
 end
 
@@ -50,8 +50,7 @@ end
 def input_students
   puts "Please enter the name of the student"
   puts "To finish, just hit return twice"
-  # create an empty array
-  students = []
+  # create an empty 
   # get the first name
   name = gets.chop
 
@@ -80,8 +79,8 @@ def input_students
     until answer_given != false
       if correct.downcase == 'y'
         # add the student hash to the array
-        students << {name: name, cohort: cohort.to_sym, hobbies: hobbies, birthplace: birthplace, age: age}
-        print_footer(students)
+        @students << {name: name, cohort: cohort.to_sym, hobbies: hobbies, birthplace: birthplace, age: age}
+        print_footer
         puts "Please enter the name of the student"
         puts "To finish, just hit return twice"
         answer_given = true
@@ -99,11 +98,58 @@ def input_students
     name = gets.chomp
   end
   # return the array of students
-  students
+  @students
 end
 
-students = input_students
-#nothing happens until we call the methods
-print_header
-print(students)
-print_footer(students)
+def show_students
+  print_header
+  print
+  print_footer
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "3. Save Students to CSV"
+  puts "9. Exit" # 9 because we'll be adding more items
+end
+
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "3"
+    save_students
+  when "9"
+    exit # this will cause the program to terminate
+  else
+    puts "I don't know what you meant, try again"
+  end
+end
+
+def save_students
+  # open the file for writing
+  file = File.open("students.csv", "w")
+  # iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort], student[:hobbies], student[:birthplace], student[:age]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
+
+def interactive_menu
+  loop do
+    #1. print menu options and as user for input
+    print_menu
+    #2. read input and save to varibale
+    process(gets.chomp)
+    #3. carry out request
+  end
+end
+
+
+interactive_menu

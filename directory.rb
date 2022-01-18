afe1ada0baa5009baf5bfab663b453c0ce0f7aec
breadmaking <liquidgoldlabel@gmail.com>
2022-01-18 09:50:53 +0000

    add in methods

--- directory.rb
+++ directory.rb
@@ -1,7 +1,5 @@
 #first we print the list of students - put into an array
 students = [
-  "The students of Villains Academy",
-  "-------------",
   "Dr. Hannibal Lecter",
   "Darth Vader",
   "Nurse Ratched",
@@ -14,10 +12,23 @@ students = [
   "Joffery Baratheon",
   "Norman Bates",
 ]
-puts "The students of Villains Academy"
-puts "-------------"
-students.each do |student|
-  puts students
+
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
 end
-#finally we print the total number of students
-print "Overall, we have #{students.count} great students"
+
+def print(names)
+  names.each do |name|
+    puts name
+  end
+end
+
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+
+#call the methods
+print_header
+print(students)
+print_footer(students)
